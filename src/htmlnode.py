5dbe461e80334f304295c2dc8b201db1793ad91c
Kaloyan Kirilov <kaloyankkirilov@gmail.com>
2026-09-22 19:50:41 +0300

    leafnode added

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,5 +1,5 @@
 class HTMLNode:
-    def __init__(self, tag=None, value=None, children=None, props=None):
+    def __init__(self, tag: str =None, value: str =None, children=None, props=None):
         self.tag = tag
         self.value = value
         self.children = children
@@ -20,4 +20,26 @@ class HTMLNode:
 
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag=tag, value=value, props=props)
+    
+    def to_html(self):
+        if self.value is None:
+            raise ValueError()
+        elif not self.tag:
+            return str(self.value)
+        else:
+            if not self.props:
+                return f'<{self.tag}>{self.value}</{self.tag}>'
+            else:
+                return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
+
+    
+
 
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 
 import unittest
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -17,6 +17,16 @@ class TestHTMLNode(unittest.TestCase):
         self.assertNotEqual(node3.value, "this is example text")
         self.assertIsNone(node3.children)
 
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+        node2 = LeafNode('a', "Push the link!", {"href": "https://www.conscious-spending.vercel.app"})
+        self.assertEqual(node2.to_html(), '<a href="https://www.conscious-spending.vercel.app">Push the link!</a>')
+        
+        node3 = LeafNode("a", None,{"href": "https://www.google.com", "target": "_blank"})
+        with self.assertRaises(ValueError):
+            node3.to_html()
 
 if __name__ == "__main__":
     unittest.main()
